df12aa0c367b947d34a06feaca403bc8c5f14f30
idzaanafin <ahmadidanafin@gmail.com>
2025-03-14 23:08:07 +0700

    soal 2 dan 4

--- soal_1/poppo_siroyo.sh
+++ soal_1/poppo_siroyo.sh
@@ -9,3 +9,6 @@ wget -q --no-check-certificate "https://docs.google.com/uc?export=download&id=$f
 #filter
 awk -F, '$2 == "Chris Hemsworth" { count++ } END { print "Chris Hemsworth membaca " count " buku." }' "$name_file"
 
+awk -F, '$8 == "Tablet" {sum+=$7; count++} END {if (count > 0) print "Rata-rata durasi membaca dengan Tablet adalah " sum/count " menit"}' "$name_file"
+
+awk -F',' 'NR > 1 {if ($7 > max) {title = $3;max = $7; name = $2}} END {print "Pembaca dengan rating tertinggi: " name " - " title  " - " max}' "$name_file"
